6d129a7d35e582fec88468b81c6e286963eed5be
mb3333mb <mengbo@asiainfo.com>
2016-11-07 17:06:37 +0800

    自动注入

--- src/main/java/com/ai/opt/data/service/atom/impl/LoginAtomSVImpl.java
+++ src/main/java/com/ai/opt/data/service/atom/impl/LoginAtomSVImpl.java
@@ -2,11 +2,11 @@ package com.ai.opt.data.service.atom.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.ai.opt.data.dao.mapper.bo.UcMembers;
 import com.ai.opt.data.dao.mapper.bo.UcMembersCriteria;
-import com.ai.opt.data.dao.mapper.factory.MapperFactory;
 import com.ai.opt.data.dao.mapper.interfaces.UcMembersMapper;
 import com.ai.opt.data.service.atom.interfaces.ILoginAtomSV;
 import com.ai.opt.sdk.util.CollectionUtil;
@@ -14,9 +14,9 @@ import com.ai.opt.sdk.util.StringUtil;
 
 @Component
 public class LoginAtomSVImpl implements ILoginAtomSV {
-	//@Autowired
-	//private SysUserMapper sysUserMapper;
 
+	@Autowired
+	private UcMembersMapper ucMembersMapper;
     @Override
     public UcMembers queryByUserName(UcMembers ucMembers) {
 
@@ -36,7 +36,7 @@ public class LoginAtomSVImpl implements ILoginAtomSV {
 //        // 删除标记为0
 //        
 //        criteria.andDelFlagEqualTo("0");
-        UcMembersMapper ucMembersMapper = MapperFactory.getUcMembersMapper();
+
        
         List<UcMembers> list = ucMembersMapper.selectByExample(conditon);
         if (!CollectionUtil.isEmpty(list)) {
